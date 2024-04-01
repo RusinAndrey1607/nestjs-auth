776c99e876313b802cb599acd81bceeaf4da835b
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-01 11:11:43 +0300

    Configure nodemailer

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   providers: [MailService],
+  imports:[
+    MailerModule.forRoot({
+      transport:{
+        host:process.env.SMTP_HOST,
+        auth:{
+          user:process.env.SMTP_USER,
+          pass:process.env.SMTP_PASSWORD
+        }
+      }
+    })
+  ],
   exports:[MailService]
 })
 export class MailModule {}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,9 +1,17 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MailService {
-    async sendMail(to:string, from:string, data:string){
-        //send mail
+    constructor(private readonly mailerService:MailerService){}
+    async sendMail(to:string, from:string, subject:string,text:string){
+        this.mailerService.sendMail({
+            to,
+            from,
+            subject,
+            text,
+            html:`<h1>Hello test message</h1>`,
+        })
         return;
     }
 }
